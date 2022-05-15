0f25d693f2edfb4325c208ea6c8babd5e849804e
JulianCotto <julian.cotto@g.austincc.edu>
2022-05-15 10:17:41 -0500

    commit

--- Program01/Code/Program01.cs
+++ Program01/Code/Program01.cs
@@ -4,33 +4,43 @@ public class HelloWorld2
 	
 	public static void Main(string[] args)
 	{
+		int incrementor = 5;
 		int counter = 0;
+		Console.WriteLine("This is a replacement program");
 		Console.WriteLine("This is Program01.");
 		Console.WriteLine("Please enter 5 days of the week");
 		Console.WriteLine("Enter your first day");
 		string dayone = Console.ReadLine();
 		if (counter <= 5)
 		{
+			incrementor = incrementor - 1;
 			counter = counter + 1;
 		}
+		Console.WriteLine("You have " + incrementor + " days left to enter");
 		Console.WriteLine("Enter your second day");
 		string daytwo = Console.ReadLine();
 		if (counter <= 5)
 		{
+			incrementor = incrementor - 1;
 			counter = counter + 1 ;
 		}
+		Console.WriteLine("You have " + incrementor + " days left to enter");
 		Console.WriteLine("Enter your third day");
 		string daythree = Console.ReadLine();
 		if (counter <= 5)
 		{
+			incrementor = incrementor - 1;
 			counter = counter + 1;
 		}
+		Console.WriteLine("You have " + incrementor + " days left to enter");
 		Console.WriteLine("Enter your fourth day");
 		string dayfour = Console.ReadLine();
 		if (counter <= 5)
 		{
+			incrementor = incrementor - 1;
 			counter = counter + 1;
 		}
+		Console.WriteLine("You have " + incrementor + " day left to enter");
 		Console.WriteLine("Enter your fifth day");
 		string dayfive = Console.ReadLine();
 		if (counter <= 5)
@@ -38,11 +48,11 @@ public class HelloWorld2
 			counter = counter + 1;
 		}
 		Console.WriteLine("You entered the following " + counter + " days:");
-		Console.WriteLine(dayone + " " + daytwo + " " + daythree + " " + dayfour + "and" + dayfive);
+		Console.WriteLine(dayone + " " + daytwo + " " + daythree + " " + dayfour + " and " + dayfive);
 		Console.WriteLine("________________________________________________");
 		
 		Console.WriteLine("Thank you for running Program01");
-		Console.WriteLine("When I first began studying for Program01 I was overuncumbered with the new vocabulary. After following all instructions and dedicating myself to replicating all examples provided, I can gladly say that Program01 was no issue for me. Thank you for providing clear cut coursework.");
+		Console.WriteLine("Program01 always should've been an easy A. Thats why I decided to go back and resubmit it. Using everything I learned over the course, I was easily able to complete it and make it even better");
 		{
 			Console.Write("Press any key to proceed");
 			Console.ReadKey(true);
